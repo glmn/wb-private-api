19197bd53d5c63e4d0a2e23916ecee090cfce973
Stanislav Gelman <glmnio@outlook.com>
2022-06-08 06:57:49 +0300

    Add docs

--- tests/WBCatalog.test.js
+++ tests/WBCatalog.test.js
@@ -3,7 +3,9 @@ const WBPrivateAPI = require('../src/WBPrivateAPI');
 
 const wbapi = new WBPrivateAPI();
 
-test('Проверка получения 100 товаров со 2 страницы по ключевому запросу "Платье"', async () => {
-  const catalog = await wbapi.search('Платье');
-  expect(catalog.page(2).length).toBe(100);
-}, 30 * 1000);
+describe('Проверка метода WBCatalog.page()', () => {
+  test('Проверка получения 100 товаров со 2 страницы по ключевому запросу "Платье"', async () => {
+    const catalog = await wbapi.search('Платье');
+    expect(catalog.page(2).length).toBe(100);
+  }, 30 * 1000);
+});
--- tests/WBPrivateAPI.test.js
+++ tests/WBPrivateAPI.test.js
@@ -3,35 +3,33 @@ const WBPrivateAPI = require('../src/WBPrivateAPI');
 
 const wbapi = new WBPrivateAPI();
 
-test('Поиск рекламодателей по ключевому слову "Платье"', async () => {
-  const ads = await wbapi.searchAds('Платье');
-  expect(ads.adverts.length).toBeGreaterThan(0);
+describe('Проверка поиска рекламодателей WBPrivateAPI.searchAds()', () => {
+  test('Присутствие не пустого свойства adverts', async () => {
+    const ads = await wbapi.searchAds('Платье');
+    expect(ads.adverts.length).toBeGreaterThan(0);
+    expect(ads.prioritySubjects.length).toBeGreaterThan(0);
+    expect(ads.adverts[0].cpm).toBeGreaterThan(0);
+    expect(ads.pages.length).toBeGreaterThan(0);
+  });
 });
 
-test('Проверка отображения CPM у рекламодателей', async () => {
-  const ads = await wbapi.searchAds('Платье');
-  expect(ads.adverts[0].cpm).toBeGreaterThan(0);
-});
+describe('Проверка поиска товаров WBPrivateAPI.search()', () => {
+  test('Поиск количества товаров по ключевому слову "Платье"', async () => {
+    const totalProducts = await wbapi.searchTotalProducts('Платье');
+    expect(totalProducts).toBeGreaterThan(0);
+  });
 
-test('Поиск количества товаров по ключевому слову "Платье"', async () => {
-  const totalProducts = await wbapi.searchTotalProducts('Платье');
-  expect(totalProducts).toBeGreaterThan(0);
-});
+  test('Проверка получения Query Params по ключевому слову "Платье"', async () => {
+    const queryParams = await wbapi._getQueryParams('Платье');
+    const [shardKey, preset, presetValue] = queryParams;
+    expect(Array.isArray(queryParams)).toBeTruthy();
+    expect(shardKey).toBe('dresses');
+    expect(preset).toBe('subject');
+    expect(presetValue).toBe('69;70;2613;2905;4000;4855;4857');
+  });
 
-test('Проверка получения shardKey и пресета по ключевому слову "Платье"', async () => {
-  const [shardKey, preset, presetValue] = await wbapi._getQueryParams('Платье');
-  expect(shardKey).toBe('dresses');
-  expect(preset).toBe('subject');
-  expect(presetValue).toBe('69;70;2613;2905;4000;4855;4857');
+  test('Сбор 100 страниц товаров по ключевому слову "Платье"', async () => {
+    const catalog = await wbapi.search('Платье');
+    expect(catalog.products.length).toBeGreaterThan(9990);
+  }, 30 * 1000);
 });
-
-test('Проверка получения shardKey и пресета по ключевому слову "Платье"', async () => {
-  const [shardKey, preset, presetValue] = await wbapi._getQueryParams('Платье');
-  const catalogPage = await wbapi.getCatalogPage({ shardKey, preset, presetValue }, 3);
-  expect(catalogPage.length).toBe(100);
-});
-
-test('Сбор 100 страниц товаров по ключевому слову "Платье"', async () => {
-  const catalog = await wbapi.search('Платье');
-  expect(catalog.products.length).toBeGreaterThan(9990);
-}, 30 * 1000);

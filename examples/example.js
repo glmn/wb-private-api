79bbb8aeef5e4afedfce9d7f7d0b0547433d3079
Stanislav Gelman <glmnio@outlook.com>
2022-08-19 07:33:44 +0300

    Remove unused packages

--- examples/example.js
+++ examples/example.js
@@ -1,11 +1,12 @@
+const Constants = require('../src/Constants');
 const WBPrivateAPI = require('../src/WBPrivateAPI');
 
 const wbapi = new WBPrivateAPI();
 
 (async () => {
+  const ads = await wbapi.getSearchAds('менструальная чаша');
+  console.log(ads.adverts);
 
-  const KEYWORD = 'менструальные чаши';
-  const ads = await wbapi.getSearchAds(KEYWORD);
-
-  console.log(ads)
+  const catalog = await wbapi.search('менструальные чаши', 1);
+  console.log(catalog)
 })();
